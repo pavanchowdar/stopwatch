74b9a5711bef56d2c9f4b6530638e583037b3a9a
sai pavan <67500798+pavanchowdar@users.noreply.github.com>
2021-10-18 16:45:44 +0545

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,19 @@
+import time
+
+print("press the ENTER key to start ")
+print("press the CTRL+c to exit timer")
+while True:
+    try:
+        input()
+        start = time.time()
+        print("Started")
+    except KeyboardInterrupt:
+        print("Stopped")
+        end = time.time()
+        print("Total Time: ", round(end - start, 2), "Seconds")
+        break
+
+
+
+
+
